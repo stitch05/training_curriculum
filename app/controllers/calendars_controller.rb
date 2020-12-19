504b400406ecf2c89a78755e1ab31130cd26db42
stitch05 <74092012+stitch05@users.noreply.github.com>
2020-12-19 19:47:57 +0900

    Issue5

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -23,6 +23,7 @@ class CalendarsController < ApplicationController
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
+    # 今日の日付＝１２月１９日という情報が取得できている
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
@@ -34,9 +35,18 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      wday_num = Date.today.wday
+      if wday_num < 7
+        wday_num = wday_num -7
+      end
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[Date.today.wday + x] }
       @week_days.push(days)
     end
-
-  end
+# ブロック変数xには、１回目には０、２回目は１、、、と数字が増加していく
+  # @todays_date + xは１回目に12月19日、２回目に12月20日、、、と増えていく 
+  # Date today.wdayでは曜日情報を数値に変えて取得する→今日だったら、土曜日なので、数値は５になる
+  # 配列wdaysの要素から添字を指定して取得する記述を行うwdays[2]
+end
 end
